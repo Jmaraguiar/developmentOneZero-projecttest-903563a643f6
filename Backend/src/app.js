598d1cebe5f1378911ef6ed05e3c01ce7593950e
jmaraguiar <jmaraguiar22@gmail.com>
2022-08-25 02:27:45 -0300

    Adição do CORS no backend

--- Backend/src/app.js
+++ Backend/src/app.js
@@ -2,6 +2,7 @@ import Koa from 'koa';
 import Router from 'koa-router';
 import YAML from 'yamljs';
 import { koaSwagger } from 'koa2-swagger-ui';
+import cors from '@koa/cors'
 
 const spec = YAML.load('./api.yaml')
 
@@ -15,6 +16,7 @@ router.get('/docs', koaSwagger({ routePrefix: false, swaggerOptions: { spec } })
 koa
   .use(router.routes())
   .use(router.allowedMethods())
+  .use(cors())
   
 const server = koa.listen(PORT);
 
